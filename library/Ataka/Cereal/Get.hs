ef48f49064a9cfedff50b4492202488c26cfb463
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-09-03 19:00:21 +0300

    Add text and bytestring decoding

--- library/Ataka/Cereal/Get.hs
+++ library/Ataka/Cereal/Get.hs
@@ -3,6 +3,7 @@ module Ataka.Cereal.Get where
 import Ataka.Prelude
 import Ataka.Types
 import Data.Serialize.Get
+import qualified Data.Text.Encoding as Text
 
 
 versionedRequestDecoding :: Word32 -> Get request -> Get (VersionedRequestDecoding request)
@@ -13,3 +14,12 @@ versionedRequestDecoding expectedVersion getRequest = do
     else do
       request <- getRequest
       return (SupportedVersionedRequestDecoding request)
+
+byteString :: Get ByteString
+byteString = getWord32le >>= getByteString . fromIntegral
+
+text :: Get Text
+text =
+  getWord32le >>=
+  getBytes . fromIntegral >>=
+  either (fail . show) return . Text.decodeUtf8'
